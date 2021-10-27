e9ff1d50ec7b3a4c1b028f5d930940f63ae439ff
Tobias Prepas <tobias.prepas@gmail.com>
2021-10-27 21:18:53 +0200

    Added data + progress bar

--- src/weather/weatherHighlight/Humidity.js
+++ src/weather/weatherHighlight/Humidity.js
@@ -6,9 +6,17 @@ const Humidity = (props) => {
     <Card className={styles.humidity}>
       <p>Humidity</p>
       <h1>
-        <span className={styles.windSpeed}>84</span>%
+        <span className={styles.windSpeed}>{props.humidity}</span>%
       </h1>
-      <input />
+      <div className={styles.percentage}>
+        <span>0</span>
+        <span>50</span>
+        <span>100</span>
+      </div>
+      <progress value={props.humidity} max="100"></progress>
+      <div className={styles.percentageSign}>
+        <span>%</span>
+      </div>
     </Card>
   );
 };
