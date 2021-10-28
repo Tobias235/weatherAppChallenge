33adedc7949feea04ea91cf1299e6de56a067dd3
Tobias Prepas <tobias.prepas@gmail.com>
2021-10-28 19:56:56 +0200

    Fixed loading data from API and send it through props to each component

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ import { useState, useEffect } from "react";
 
 import MainWeather from "./weather/MainWeather";
 import SearchBar from "./weather/SearchBar";
+import Modal from "./weather/modal/Modal";
 
 function App() {
+  const [coords, setCoords] = useState({
+    lat: "59.3293",
+    lon: "18.0686",
+  });
+  const [showModal, setShowModal] = useState(false);
   const [currentWeather, setCurrentWeather] = useState({});
   const [highlights, setHighlights] = useState({});
+  const [forecast, setForecast] = useState([]);
 
-  useEffect(() => {
-    fetchData();
-  }, []);
-  const fetchData = async () => {
+  let array = [];
+
+  const fetchLocationCoords = async (location) => {
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=stockholm&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric
-    `
+      `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric
+      `
     );
 
     if (!response.ok) {
@@ -21,30 +27,72 @@ function App() {
     }
     const responseData = await response.json();
     console.log(responseData);
-    setCurrentWeather({
-      city: responseData.name,
-      temperature: responseData.main.temp,
-      weather: responseData.weather[0].main,
-    });
-    setHighlights({
-      wind: responseData.wind.speed,
-      windDirection: responseData.wind.deg,
-      humidity: responseData.main.humidity,
-      visibility: responseData.visibility,
-      airPressure: responseData.main.pressure,
+    setCoords({
+      lat: responseData.coord.lat,
+      lon: responseData.coord.lon,
     });
   };
 
+  useEffect(() => {
+    const fetchWeatherData = async (location) => {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${coords.lat}&lon=${coords.lon}&exclude=hourly,minutely&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+      const responseData = await response.json();
+      const city = responseData.timezone.split("/");
+      setCurrentWeather({
+        city: city[1],
+        temperature: responseData.current.temp,
+        weather: responseData.current.weather[0].description,
+        icon: responseData.current.weather[0].icon,
+      });
+      setHighlights({
+        wind: responseData.current.wind_speed,
+        windDirection: responseData.current.wind_deg,
+        humidity: responseData.current.humidity,
+        visibility: responseData.current.visibility,
+        airPressure: responseData.current.pressure,
+      });
+      array = [...responseData.daily];
+      setForecast(array);
+    };
+
+    fetchWeatherData();
+  }, [coords]);
+
   const handleClick = () => {
-    console.log(currentWeather);
-    console.log(highlights);
+    console.log(forecast);
+  };
+
+  const handleSearchButton = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  const handleSearchLocation = (e) => {
+    const parent = e.currentTarget.parentElement;
+    fetchLocationCoords(parent.children[0].value);
+    setShowModal(false);
   };
 
   return (
     <div className="container">
       <button onClick={handleClick}>click me</button>
-      <SearchBar weatherData={currentWeather} />
-      <MainWeather weatherHighlights={highlights} />
+      <SearchBar
+        weatherData={currentWeather}
+        onHandleModal={handleSearchButton}
+      />
+      {showModal && (
+        <Modal onClick={handleCloseModal} onSearch={handleSearchLocation} />
+      )}
+      <MainWeather weatherHighlights={highlights} forecast={forecast} />
     </div>
   );
 }
