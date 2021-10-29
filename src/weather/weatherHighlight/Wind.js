469fcf20312b358f9e58ddd3c3d450eb1beb9cb6
Tobias Prepas <tobias.prepas@gmail.com>
2021-10-29 13:12:36 +0200

    Fixed wind direction icon

--- src/weather/weatherHighlight/Wind.js
+++ src/weather/weatherHighlight/Wind.js
@@ -2,6 +2,29 @@ import Card from "../../UI/Card";
 import styles from "./Wind.module.css";
 
 const Wind = (props) => {
+  let compass = [
+    "N",
+    "NNE",
+    "NE",
+    "ENE",
+    "E",
+    "ESE",
+    "SE",
+    "SSE",
+    "S",
+    "SSW",
+    "SW",
+    "WSW",
+    "W",
+    "WNW",
+    "NW",
+    "NNW",
+    "N",
+  ];
+  const windDirection = compass[(props.wind.direction / 22.5).toFixed(0)];
+  const style = {
+    transform: `rotate(${props.wind.direction}deg)`,
+  };
   return (
     <Card className={styles.wind}>
       <p>Wind Status</p>
@@ -9,7 +32,10 @@ const Wind = (props) => {
         <span className={styles.windSpeed}>{props.wind.speed}</span>m/s
       </h1>
       <div>
-        <span className="material-icons">navigation</span> <span>WSW</span>
+        <span style={style} className="material-icons">
+          navigation
+        </span>
+        <span>{windDirection}</span>
       </div>
     </Card>
   );
